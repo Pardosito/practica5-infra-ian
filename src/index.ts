fec3ae600d31a1cefb17667bb36ecd7c4826ebf7
Pardosito <ianrdzwong@gmail.com>
2026-09-17 21:12:04 -0600

    chore: updating deploy workflow and app response

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		return new Response("Deployed with GitHub Actions! Testo testo, o algo así...");
 	},
 } satisfies ExportedHandler<Env>;
